54326fd881ead4918803f47dc260c28a35901a65
Amr Saber <amr.m.saber.mail@gmail.com>
2022-05-31 11:16:34 +0200

    add a test to test synced writes

--- dequeue_integration_test.go
+++ dequeue_integration_test.go
@@ -79,7 +79,61 @@ func TestSyncedPops(t *testing.T) {
 			v1 := dequeue.PopFront()
 			v2 := dequeue.PopBack()
 
-			// This only locks the results slice so PopFront is being tested correctly
+			// This only locks the results slice so PopFront and PopBack are being tested correctly
+			resultLock.Lock()
+			defer resultLock.Unlock()
+
+			results = append(results, v1, v2)
+		}()
+	}
+
+	wg.Wait()
+
+	// Make sure that all the number from values are in results
+	times := make(map[int]int)
+	for _, value := range results {
+		times[value]++
+	}
+
+	for value, count := range times {
+		if count != 2 {
+			t.Errorf("Expected %d to be in results twice, got %d", value, count)
+		}
+	}
+}
+
+// Test that when reading and writing items at the same time, no value is lost.
+func TestSyncedMixedWrites(t *testing.T) {
+	values := []int{}
+	for i := 1; i <= 1000; i++ {
+		values = append(values, i)
+	}
+	results := make([]int, 0, len(values))
+	resultLock := sync.Mutex{}
+
+	dequeue := NewBlockingDequeue[int]()
+	wg := sync.WaitGroup{}
+
+	// Concurrent producers
+	for i := 0; i < len(values); i++ {
+		wg.Add(1)
+		go func(v int) {
+			defer wg.Done()
+			dequeue.PushBack(v)
+			dequeue.PushFront(v)
+		}(values[i])
+	}
+
+	// Concurrent consumers
+	for i := 0; i < len(values); i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			v1 := dequeue.PopFront()
+			v2 := dequeue.PopBack()
+
+			// This only locks the results slice so PopFront and PopBack are being tested correctly
 			resultLock.Lock()
 			defer resultLock.Unlock()
 
